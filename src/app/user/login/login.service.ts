2f3285cc71c4a00af510d4e42282867b3559845d
Luci Furtun <lucianfurtun@gmail.com>
2017-11-14 22:48:28 +0200

    Update requirements

--- src/app/user/login/login.service.ts
+++ src/app/user/login/login.service.ts
@@ -1,9 +1,8 @@
 import {Injectable} from '@angular/core';
 import {ApiService} from 'app/api.service';
-import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {HttpClient} from '@angular/common/http';
 import {apiPoints} from '../../../environments/environment';
 import {Router} from '@angular/router';
-import {Login} from 'app/user/login/login';
 
 @Injectable()
 export class LoginService extends ApiService {
